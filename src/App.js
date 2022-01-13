021f9af481725a0ab720245e9bb460c60c8699ee
tangentdigitalagency <david@tangentdigitalagency.com>
2022-01-13 14:37:53 -0600

    zip

--- src/App.js
+++ src/App.js
@@ -53,7 +53,9 @@ class App extends Component {
       legal_business_name: '',
       business_website: '',
       address: '',
-      zip_code: '',
+      zip_code: localStorage.getItem('zip'),
+      city: localStorage.getItem('city'),
+      state: localStorage.getItem('state'),
       business_structure: '',
       ein: 'Yes',
       business_profession: '',
@@ -96,6 +98,8 @@ class App extends Component {
                       jornaya_lead_id: document.getElementById('leadid_token').value,
                       trusted_form_cert_id: document.getElementById('xxTrustedFormToken_0').value,
                       zip_code: v,
+                      city: localStorage.getItem('city'),
+                      state: localStorage.getItem('state')
                     },
                   });
                 }}
